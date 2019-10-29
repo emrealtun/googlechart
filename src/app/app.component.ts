35230b97ad25f3bf5fd444198a24fb7c8d3d4d54
emrealtun <altunemre6@gmail.com>
2019-10-29 22:46:11 +0300

    google timeline chart created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Time } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'barchartt';
+
+  public isim:string = "EMRE"
+  public isim2:string = "FATİH"
+  public Time2:number= Date.now()
+
+
+  
+  public timelineChartData:any =  {
+    chartType: 'Timeline',
+    dataTable:
+    [
+
+      ['Name', 'From', 'BeginTo','Endto'],
+      [ this.isim,"Intermediate JavaScript",        new Date(0,0,0,12,0,0),  new Date(0,0,0,13,30,0) ],
+      [ this.isim, 'Intermediate JavaScript',    new Date(0,0,0,14,0,0),  new Date(0,0,0,15,30,0) ],
+      [ this.isim, 'Advanced JavaScript',        new Date(0,0,0,16,0,0),  new Date(0,0,0,17,30,0) ],
+      [ this.isim2,   'Beginning Google Charts',    new Date(0,0,0,12,30,0), new Date(0,0,0,14,0,0) ],
+      [ this.isim2,   'Intermediate Google Charts', new Date(0,0,0,14,30,0), new Date(0,0,0,16,0,0) ],
+      [ this.isim2,   'Advanced Google Charts',     new Date(0,0,0,16,30,0), new Date(0,0,0,18,0,0) ]
+
+    ],
+    options: {
+      timeline: { singleColor: '#566',fontName:'Helvetica'},
+      backgroundColor: '#F1E9E8'
+    }
+
+    
+ }
+
 }
